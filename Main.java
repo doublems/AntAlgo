1eca63dfbaf31552a9b5728f1d70ac2ef8863a2f
jerry <jb7959@naver.com>
2018-07-21 00:07:04 +0900

    첫번째 작성 - 리팩토링 필요

--- Main.java
+++ Main.java
@@ -17,16 +17,16 @@
 
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("END"+selectNum(10));
+        Main main = new Main();
+        System.out.println("END             :"+main.selectNum(5));
     }
 
-    public static int selectNum(int endNum){
+    public Queue selectNum(int endNum){
         // 큐에 집어넣어서 순차적으로 확인해야함
         // 순차적으로 활용은 '연속' 이라는 점에 집중해야함
         // 담을 그릇이 필요함 (두개사용하자)
@@ -34,55 +34,40 @@ public class Main {
         Queue<Integer> bfAntQueue = new ArrayDeque<Integer>();
         Queue<Integer> afAntQueue = new ArrayDeque<Integer>();
 
-        for(int i = 0; i<endNum; i++){
-            //첫 시작일경우 1을 큐에 추가한다.
-            if(i == 0){
+        for(int i = 0;i<endNum;i++){
+            if(i==0){
                 bfAntQueue.add(1);
-            }
-
-            VoAntType voAntType = new VoAntType();
-
-
-            //작업대상 큐가 비어질 때 까지 동작 또는 사이즈가 endNum보다 작을때까지
-            while (!bfAntQueue.isEmpty() || !(bfAntQueue.size()<=endNum)){
-                
-                //꺼내기
-                int eachPreData = bfAntQueue.poll();
-                //다른 숫자가 나타나면 저장 후 voAntType 업데이트
-                //System.out.println("eachPreData"+eachPreData);
-                //System.out.println("voAntType.getType()"+voAntType.getType());
-                //System.out.println(bfAntQueue.size());
-
-                if(eachPreData != voAntType.getType()){
-                    //저장
-                    afAntQueue.add(voAntType.getAmount());
-                    afAntQueue.add(voAntType.getType());
-                    //voAntType 업데이트
-                    voAntType.setType(eachPreData);
-                    voAntType.setAmount(0);
-                }else{
-                    //같은 숫자인 경우 수량 증가
-                    voAntType.setAmount((voAntType.getAmount())+1);
-                    //System.out.println(voAntType.getAmount());
-                    if(bfAntQueue.size()==0){
-                        System.out.println(voAntType.getAmount());
-                        System.out.println(voAntType.getType());
-                        //저장
-                        afAntQueue.add(voAntType.getAmount());
-                        afAntQueue.add(voAntType.getType());
+                afAntQueue.add(1);
+            }else {
+                VoAntType voAntType = new VoAntType();
+                for(int j = bfAntQueue.size();j>0;j--){
+                    int type = bfAntQueue.poll();
+                    if(type == voAntType.getType()){
+                        voAntType.setAmount(voAntType.getAmount() + 1);
+                    }else {
+                        if(voAntType.getAmount()!=0){
+                            afAntQueue.add(voAntType.getAmount());
+                            afAntQueue.add(voAntType.getType());
+                        }
+                        voAntType.setType(type);
+                        voAntType.setAmount(1);
                     }
                 }
+                afAntQueue.add(voAntType.getAmount());
+                afAntQueue.add(voAntType.getType());
             }
-            System.out.println("i:"+i);
-            //작업 후의 큐를 작업 전 큐에 반영
-            //bfAntQueue = afAntQueue;
+            bfAntQueue = afAntQueue;
         }
 
-        return afAntQueue.size();
+        return afAntQueue;
     }
 
+
+
+
+
     //this class is VO for Ant
-    private static class VoAntType{
+    private class VoAntType{
         private int amount = 0;
         private int type = 1;
 
